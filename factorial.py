08f73e8eaec48627ad48e35f4febb3857d282e61
Imesha Kuruppu <imeshank@gmail.com>
2021-07-23 19:27:58 -0700

    adding python file

---
+++ factorial.py
@@ -0,0 +1,21 @@
+# finding factorial
+
+def factorial(n):
+    if n < 0:
+        return 0
+    elif n == 0 or n ==1:
+        return 1
+    else:
+        fact = 1
+        while (n>1):
+            fact *= n
+            n -= 1
+        return fact
+
+def main():
+    # num = 5;
+    num = int(input("\n Enter a number: \n"))
+    print("Factorial of {} is {}".format(num, factorial(num)))
+
+if __name__ == "__main__":
+    main()
